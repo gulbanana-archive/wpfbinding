cad14524b5d0616335f4051ab9693148f12c4ef1
Thomas Castiglione <thomas@agcom.com.au>
2014-08-22 19:28:38 +0800

    switch from using StartupUri to a basic controller

--- App.xaml.cs
+++ App.xaml.cs
@@ -13,5 +13,9 @@ namespace wpfbinding
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            new Controller();
+        }
     }
 }
---
+++ Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpfbinding
+{
+    class Controller
+    {
+        public Controller()
+        {
+            var w = new MainWindow();
+            w.Show();
+        }
+    }
+}
