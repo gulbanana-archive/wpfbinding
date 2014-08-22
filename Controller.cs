b59719eb57102fc5bf21736098929981d8fa9561
Thomas Castiglione <thomas@agcom.com.au>
2014-08-22 19:39:48 +0800

    create viewmodel classes and set them as the datacontext of the view classes, used for databinding

--- Controller.cs
+++ Controller.cs
@@ -11,7 +11,10 @@ namespace wpfbinding
         public Controller()
         {
             var w1 = new View1();
+            w1.DataContext = new Model1();
+
             var w2 = new View2();
+            w2.DataContext = new Model2();
 
             w1.Show();
             w2.Show();
