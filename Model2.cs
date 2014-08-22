d48cb22351b6412700e20c3847707facce0e6cee
Thomas Castiglione <thomas@agcom.com.au>
2014-08-22 20:15:30 +0800

    remove and sort

--- Model2.cs
+++ Model2.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.Practices.Prism.Mvvm;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace wpfbinding
 {
